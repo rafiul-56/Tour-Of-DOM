e2745934bc194a4a1094fca662362dc2badf93e4
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-08-14 00:29:15 +0600

    practice

--- JS/second.js
+++ JS/second.js
@@ -1,9 +1,1 @@
 console.log('second file');
-
-const secondList = document.getElementById('second-list');
-
-// 
-const li = document.createElement('li');
-li.innerText = 'My dynamic item'
-
-secondList.appendChild(li);
